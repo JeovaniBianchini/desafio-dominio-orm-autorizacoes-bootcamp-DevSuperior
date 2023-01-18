b7fc48ba910a297199a3befdca12b27fa93b5181
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2023-01-18 14:12:12 -0400

    Implementation of the domain/seed model in the database/implementation of UserDetails and UserDetailsService

---
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(name);
+
+        if (user == null){
+            throw new UsernameNotFoundException("UserName not found");
+        }
+        return user;
+    }
+}
